425996c8ca9a9d3ad08321e3508fc8aa6e062d2f
seinada10 <48283791+seinada10@users.noreply.github.com>
2025-03-05 23:47:19 -0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'Anti-Limbo: Evita quedas no limbo e teleporta para a garagem mais próxima'
+version '1.0.0'
+
+author 'seinada10'
+
+client_scripts {
+	'config/config.lua',
+	'client.lua'
+}
+
+server_scripts {
+	'config/config.lua',
+	'server.lua'
+}
